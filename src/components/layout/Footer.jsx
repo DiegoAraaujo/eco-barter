cd4b39f1c6d441034b08bc7eb2c1fa7e2b9fae7a
Sâmia Luvanice <samia.luvanice.dev@gmail.com>
2025-07-26 16:19:55 -0300

    feat: Add Footer component

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../styles/components/Footer.css';
+
+const Footer = () => {
+  return (
+    <footer role="contentinfo" className="footer">
+      <p>EcoBarter — Conectando pessoas por meio de trocas conscientes.</p>
+    </footer>
+  );
+};
+
+export default Footer;
