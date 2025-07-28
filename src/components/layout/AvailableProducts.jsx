dc58db1b1c56e93fc32d72dab6e37b4130048f40
DiegoAraaujo <diegoaraaujo8@gmail.com>
2025-07-28 15:24:28 -0300

    refactor: refactor AvailableProducts component and change visuals

--- src/components/layout/AvailableProducts.jsx
+++ src/components/layout/AvailableProducts.jsx
@@ -2,40 +2,44 @@ import "../../styles/availableProducts.css";
 import CardProduct from "../ui/CardProduct";
 
 // imagem dos produtos
-import ImgCamisa from "../../assets/image.svg";
+import Item1 from "../../assets/item1.svg";
+import Item2 from "../../assets/item2.svg";
+import Item3 from "../../assets/item3.svg";
+import Item4 from "../../assets/item4.svg";
 function AvailableProducts() {
+
   return (
     <section className="products-section">
       <h2>Itens disponíveis para troca</h2>
 
       <div className="Products-list">
         <CardProduct
-          image={ImgCamisa}
+          image={Item1}
           imageDescription="camisa masculina"
           productName="Jaqueta"
           location="Sobral-CE"
           productState="Estado de novo, nunca usada."
         />
         <CardProduct
-          image={ImgCamisa}
-          imageDescription="camisa masculina"
-          productName="Jaqueta"
-          location="Sobral-CE"
-          productState="Estado de novo, nunca usada."
+          image={Item2}
+          imageDescription="Escrivaninha simples"
+          productName="Escrivaninha"
+          location="Maracanaú-CE"
+          productState="Na caixa ainda, nova."
         />
         <CardProduct
-          image={ImgCamisa}
-          imageDescription="camisa masculina"
-          productName="Jaqueta"
-          location="Sobral-CE"
-          productState="Estado de novo, nunca usada."
+          image={Item3}
+          imageDescription="alexa echo show 5"
+          productName="Alexa echo show 5"
+          location="Fortaleza-CE"
+          productState="Semi-nova, pra vender logo."
         />
         <CardProduct
-          image={ImgCamisa}
-          imageDescription="camisa masculina"
-          productName="Jaqueta"
-          location="Sobral-CE"
-          productState="Estado de novo, nunca usada."
+          image={Item4}
+          imageDescription="Iphone 15"
+          productName="Iphone 15"
+          location="Ubajara-CE"
+          productState="Com poucas marcas de uso."
         />
       </div>
     </section>
