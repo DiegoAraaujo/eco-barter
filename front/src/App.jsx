604a9fc4139fcd4b088978aa8162c98fb804e6b1
DiegoAraaujo <diegoaraaujo8@gmail.com>
2025-08-26 14:32:05 -0300

    feat(context): create UserContext and useUserContext hook

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,18 +1,25 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState } from "react";
 
+import { UserContext } from "./contexts/UserContext";
 import WelcomePage from "./pages/WelcomePage";
 import Login from "./pages/Login";
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+import MyArea from "./pages/MyArea";
 
-      {/* <MyArea /> */}
-    </BrowserRouter>
+function App() {
+  const [user, setUser] = useState(null);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/myarea" element={<MyArea />} />
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
